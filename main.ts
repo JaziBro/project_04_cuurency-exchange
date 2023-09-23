7f13831d0b7b089bff2b64ce59751ab7a8956a7d
jazilhashmi <138767638+JaziBro@users.noreply.github.com>
2023-09-23 08:06:54 -0700

    .

--- main.ts
+++ main.ts
@@ -1,22 +1,41 @@
 #! usr/bin/env Node
 import inquirer from "inquirer"
 
+let conversions = {
+    "PKR": {
+        "PKR": 1,
+        "USD": 0.0035,
+        "INR": 0.29
+    },
+    "USD": {
+        "USD": 1,
+        "PKR": 287.22,
+        "INR": 83.10
+    },
+    "INR": {
+        "INR": 1,
+        "PKR": 3.46,
+        "USD": 83.10
+    },
+
+}
+
 let answers = await inquirer.prompt([
     {
         name: "from",
         type: "list",
         choices: ["PKR","USD","INR"],
-        message: "Choose the Currency you want to convert"
+        message: "Choose the Currency you want to convert: "
     },
     {
         name: "to",
         type: "list",
         choices: ["PKR","USD","INR"],
-        message: "Choose the Currency you want to convert "
+        message: "Choose the Currency you want to convert: "
     },
     {
         name: "amount",
         type: "number",
-        message: "Enter your amount"
+        message: "Enter your amount: "
     },
 ])
