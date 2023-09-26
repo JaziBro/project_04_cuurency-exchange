323c86299df262cbfe4878d9d5e8edbc61319111
jazilhashmi <138767638+JaziBro@users.noreply.github.com>
2023-09-26 11:29:24 -0700

    .

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 #! usr/bin/env Node
 import inquirer from "inquirer";
-let conversions = {
+let conversion = {
     "PKR": {
         "PKR": 1,
-        "USD": 0.0035,
+        "USD": 0.0034,
         "INR": 0.29
     },
     "USD": {
         "USD": 1,
-        "PKR": 287.22,
-        "INR": 83.10
+        "PKR": 290.50,
+        "INR": 83.29
     },
     "INR": {
         "INR": 1,
-        "PKR": 3.46,
-        "USD": 83.10
-    },
+        "USD": 0.012,
+        "PKR": 3.49
+    }
 };
 let answers = await inquirer.prompt([
     {
         name: "from",
         type: "list",
         choices: ["PKR", "USD", "INR"],
-        message: "Choose the Currency you want to convert: "
+        message: "Choose the Currency you want to convert from: "
     },
     {
         name: "to",
         type: "list",
         choices: ["PKR", "USD", "INR"],
-        message: "Choose the Currency you want to convert: "
+        message: "Choose the Currency you want to convert to: "
     },
     {
         name: "amount",
@@ -36,3 +36,8 @@ let answers = await inquirer.prompt([
         message: "Enter your amount: "
     },
 ]);
+const { from, to, amount } = answers;
+if (from && to && amount) {
+    let result = conversion[from][to] * amount;
+    console.log(`The conversion amount of ${amount} ${from} to ${to} is ${result}`);
+}
--- main.ts
+++ main.ts
@@ -1,37 +1,42 @@
 #! usr/bin/env Node
 import inquirer from "inquirer"
 
-let conversions = {
+let conversion = {
     "PKR": {
         "PKR": 1,
-        "USD": 0.0035,
+        "USD": 0.0034,
         "INR": 0.29
     },
-    "USD": {
+    "USD":{
         "USD": 1,
-        "PKR": 287.22,
-        "INR": 83.10
+        "PKR": 290.50,
+        "INR": 83.29
     },
     "INR": {
         "INR": 1,
-        "PKR": 3.46,
-        "USD": 83.10
-    },
-
+        "USD": 0.012,
+        "PKR": 3.49
+    }
 }
 
-let answers = await inquirer.prompt([
+let answers: {
+
+    from: "PKR" | "USD" | "INR",
+    to: "PKR" | "USD" | "INR",
+    amount: number
+
+} = await inquirer.prompt([
     {
         name: "from",
         type: "list",
         choices: ["PKR","USD","INR"],
-        message: "Choose the Currency you want to convert: "
+        message: "Choose the Currency you want to convert from: "
     },
     {
         name: "to",
         type: "list",
         choices: ["PKR","USD","INR"],
-        message: "Choose the Currency you want to convert: "
+        message: "Choose the Currency you want to convert to: "
     },
     {
         name: "amount",
@@ -39,3 +44,10 @@ let answers = await inquirer.prompt([
         message: "Enter your amount: "
     },
 ])
+
+const {from, to, amount} = answers
+if (from && to && amount){
+    let result = conversion[from][to] * amount
+    console.log(`The conversion amount of ${amount} ${from} to ${to} is ${result}`)
+
+}
